b2a8b477fa2e63aa983596e13ef8e81b09ef2265
aiexq <68229865+aiexq@users.noreply.github.com>
2021-06-17 15:32:03 +0300

    fghhhhhh

--- src/utilities/configFiles/Configuration.java
+++ src/utilities/configFiles/Configuration.java
@@ -3,5 +3,5 @@ package utilities.configFiles;
 public class Configuration {
     public static String DBURL = "jdbc:mysql://localhost:3306/crm";
     public static String DBUSER = "root";
-    public static String DBPASSWORD = "";
+    public static String DBPASSWORD = "root";
 }
